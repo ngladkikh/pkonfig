7ba73396cbfd3076345736bfcb4295a202be6a07
Nikita Gladkikh <gladkikh.nikita@gmail.com>
2025-10-04 13:20:44 -0400

    Take a package version from the git tag

--- pkonfig/__init__.py
+++ pkonfig/__init__.py
@@ -1,3 +1,5 @@
+from importlib import metadata as _metadata
+
 from pkonfig.config import Config
 from pkonfig.errors import ConfigError, ConfigTypeError, ConfigValueNotFoundError
 from pkonfig.fields import (
@@ -17,3 +19,31 @@ from pkonfig.fields import (
     Str,
 )
 from pkonfig.storage import *
+
+# Expose package version for documentation and consumers
+try:
+    __version__ = _metadata.version("pkonfig")
+except _metadata.PackageNotFoundError:  # pragma: no cover - not in an installed context
+    __version__ = "0.0.0"
+
+__all__ = [
+    "Config",
+    "ConfigError",
+    "ConfigTypeError",
+    "ConfigValueNotFoundError",
+    "Bool",
+    "Byte",
+    "ByteArray",
+    "Choice",
+    "DecimalField",
+    "EnumField",
+    "Field",
+    "File",
+    "Float",
+    "Folder",
+    "Int",
+    "LogLevel",
+    "PathField",
+    "Str",
+    "__version__",
+]
